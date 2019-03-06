51eae942723d8b878d6e167e66ac23a0645c6ef7
Rabiga Sarbassova <iamrabiga@gmail.com>
2019-03-06 13:04:35 +0600

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-// for Security connection
-var forceSsl = require('force-ssl-heroku');
-app.use(forceSsl);
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/algortemp'));
 
